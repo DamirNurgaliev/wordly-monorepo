9bed0f1324a51b5f393e04f1e0d2ff1a22ccde6b
Damir Nurgaliev <damir.nurgaliev@flatstack.com>
2023-11-05 19:24:22 +0100

    wip

--- apps/wordly-spa/src/app/hooks/useLettersColor.ts
+++ apps/wordly-spa/src/app/hooks/useLettersColor.ts
@@ -2,33 +2,39 @@ import { useEffect } from 'react';
 import { useImmer } from 'use-immer';
 import { GameField, LettersColor } from '../commonTypes';
 
+const initialState = {
+  green: [],
+  orange: [],
+  grey: [],
+};
+
 const useLettersColor = (gameField: GameField[], currentAttempt: number) => {
-  const [lettersColor, setLettersColor] = useImmer<LettersColor>({
-    green: [],
-    orange: [],
-    grey: [],
-  });
+  const [lettersColor, setLettersColor] = useImmer<LettersColor>(initialState);
 
   useEffect(() => {
-    setLettersColor((draft) => {
-      gameField[currentAttempt - 1]?.word.split('').forEach((char, index) => {
-        if (gameField[currentAttempt - 1].guessedPositions.includes(index)) {
-          if (!draft.green.includes(char)) {
-            draft.green.push(char);
-          }
-        } else {
-          if (gameField[currentAttempt - 1].guessedLetters.includes(index)) {
-            if (!draft.orange.includes(char)) {
-              draft.orange.push(char);
+    if (currentAttempt === 0) {
+      setLettersColor(initialState);
+    } else {
+      setLettersColor((draft) => {
+        gameField[currentAttempt - 1]?.word.split('').forEach((char, index) => {
+          if (gameField[currentAttempt - 1].guessedPositions.includes(index)) {
+            if (!draft.green.includes(char)) {
+              draft.green.push(char);
             }
           } else {
-            if (!draft.grey.includes(char)) {
-              draft.grey.push(char);
+            if (gameField[currentAttempt - 1].guessedLetters.includes(index)) {
+              if (!draft.orange.includes(char)) {
+                draft.orange.push(char);
+              }
+            } else {
+              if (!draft.grey.includes(char)) {
+                draft.grey.push(char);
+              }
             }
           }
-        }
+        });
       });
-    });
+    }
   }, [currentAttempt]);
 
   return lettersColor;
